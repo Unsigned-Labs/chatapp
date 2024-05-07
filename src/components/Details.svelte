408d2097c02992e19ce44bf384dc815ed8868132
keraliss <keralis1131f@gmail.com>
2024-05-07 19:47:25 +0530

    basic ui structure and add tailwind
    Signed-off-by: keraliss <keralis1131f@gmail.com>

---
+++ src/components/Details.svelte
@@ -0,0 +1,42 @@
+<script>
+    let admins = [
+        { name: 'Aaron', image: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' },
+    ];
+    let online = [
+        { name: 'Benoît', image: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' },
+        { name: 'Christopher', image: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' },
+    ];
+    let offline = [
+        { name: 'Damian', image: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' },
+        { name: 'Fabio', image: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' },
+        { name: 'Hugo', image: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png' },
+    ];
+
+</script>
+<div class="w-[15%] overflow-scroll no-scrollbar bg-slate-700">
+    <ul >
+        <li class="text-gray-400 px-5 py-2 font-bold">Admins</li>
+        {#each admins as { name, image }}
+            <li class="text-white px-5 py-2 cursor-pointer">
+                <img class="w-6 h-6 rounded-full inline mr-2" src={image} alt={name} title={name} />
+                {name}
+            </li>
+        {/each}
+
+        <li class="text-gray-400 px-5 py-2 font-bold ">Online</li>
+        {#each online as { name, image }}
+            <li class="text-white px-5 py-2 cursor-pointer">
+                <img class="w-6 h-6 rounded-full inline mr-2" src={image} alt={name} title={name} />
+                {name}
+            </li>
+        {/each}
+
+        <li class="text-gray-400 px-5 py-2 font-bold">Offline</li>
+        {#each offline as { name, image }}
+            <li class="text-white px-5 py-2 cursor-pointer">
+                <img class="w-6 h-6 rounded-full inline mr-2" src={image} alt={name} title={name} />
+                {name}
+            </li>
+        {/each}
+    </ul>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Details from '../components/Details.svelte';
+import MessageWindow from '../components/MessageWindow.svelte';
+	import ServerList from '../components/ServerList.svelte';
+	import Sidebar from '../components/Sidebar.svelte';
+</script>
+
+<div class="flex h-full text-white">
+	<ServerList />
+	<Sidebar />
+	<MessageWindow />
+    <Details />
+</div>
