28f3bce5bb30de766a6a42f78d74247dc7f137d1
thejaysharma <jai3500000@gmail.com>
2024-05-20 13:45:36 +0530

    refactor: remove other pr files

--- src/utils/helpers/loginHelper.ts
+++ src/utils/helpers/loginHelper.ts
@@ -1,40 +1,7 @@
 import { get as getStore } from 'svelte/store';
 import ndkStore, { bunkerNDKStore } from '../../stores/provider';
-import { NDKNip07Signer, NDKNip46Signer, NDKPrivateKeySigner, NDKUser } from '@nostr-dev-kit/ndk';
+import { NDKNip46Signer, NDKPrivateKeySigner, NDKUser } from '@nostr-dev-kit/ndk';
 import { localStore, ndkUser } from '../../stores/stores';
-
-export async function NDKlogin(): Promise<NDKUser | undefined> {
-	const $ndk = getStore(ndkStore);
-	const signer = new NDKNip07Signer();
-	$ndk.signer = signer;
-	ndkStore.set($ndk);
-	const ndkCurrentUser = await signer.user();
-	const user = $ndk.getUser({
-		pubkey: ndkCurrentUser.pubkey,
-		npub: ndkCurrentUser.npub
-	});
-	ndkUser.set(user);
-	localStore.set({ lastUserLogged: ndkCurrentUser.npub, pk: undefined });
-	return user;
-}
-
-export async function privkeyLogin(pk: string): Promise<NDKUser | undefined> {
-	if (!pk) return undefined;
-
-	const $ndk = getStore(ndkStore);
-	const signer = new NDKPrivateKeySigner(pk);
-	$ndk.signer = signer;
-	ndkStore.set($ndk);
-	const ndkCurrentUser = await signer.user();
-	const user = $ndk.getUser({
-		pubkey: ndkCurrentUser.pubkey,
-		npub: ndkCurrentUser.npub
-	});
-	console.log(user);
-	ndkUser.set(user);
-	localStore.set({ lastUserLogged: ndkCurrentUser.npub, pk });
-	return user;
-}
 
 export async function nsecBunkerLogin(nip46ConnectionString: string): Promise<NDKUser | undefined> {
 	const $ndk = getStore(ndkStore);
